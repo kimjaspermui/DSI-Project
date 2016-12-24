64e280e8663a5d941c07bfca2f6897e26665e9e0
Jasper Mui <kmui@ucsd.edu>
2016-12-23 20:52:03 -0800

    Preparation of the project, first start with ArrayList

---
+++ MyArrayList.java
@@ -0,0 +1,17 @@
+/**
+ * Name:            Kim Jasper Mui, Erick Soto, Tiffany Pan
+ * Date:            Dec 23 2016
+ * Filename:        MyArrayList.java
+ * Description:     Partial implementation of ArrayList using int as the data
+ *                  type of each element for backend structures of the GUI
+ *                  representation. It contains functionality for add, find,
+ *                  and remove.
+ * Sources of help: 
+ */
+
+import java.util.*;
+
+public class MyArrayList {
+
+
+}
