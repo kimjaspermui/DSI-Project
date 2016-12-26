299071eea8197c4aadf652eaca2ad8cc0966839f
Jasper Mui <kmui@ucsd.edu>
2016-12-25 23:22:41 -0800

    Re-position the texts, then made the data structures text hoverable (and actually clickable now), next is to implement some operations

--- MyController.java
+++ MyController.java
@@ -18,8 +18,8 @@ import javax.swing.*;
  * This class contains the instructions to create the Graphical User Interface
  * layout for the menu and controller.
  */
-public class MyController extends WindowController implements ActionListener {
-// implements MouseListener
+public class MyController extends WindowController implements ActionListener,
+MouseListener, MouseMotionListener {
 
   private static final int HALF = 2;
 
@@ -29,6 +29,7 @@ public class MyController extends WindowController implements ActionListener {
 
   // menu texts specifications
   private static final Color MENU_FONT_COLOR = Color.BLACK;
+  private static final Color MENU_FONT_HOVER_COLOR = Color.BLUE;
   private static final int MENU_FONT_SIZE = 50;
   private static final int Y_GAP = 20;
 
@@ -52,6 +53,14 @@ public class MyController extends WindowController implements ActionListener {
 
   private static final int TEXT_FIELD_SIZE = 5;
 
+  // the boolean to indicate whether or not the user is at the menu
+  private boolean onMenu;
+
+  private boolean textHover;
+
+  // selection assigments
+  private static final int ARRAYLIST = 0;
+
   private MyDataStructure myDS;
 
   /**
@@ -61,13 +70,17 @@ public class MyController extends WindowController implements ActionListener {
    */
   public void begin() {
 
+    onMenu = true;
+    textHover = false;
+
+    canvas.addMouseListener(this);
+    canvas.addMouseMotionListener(this);
+
     setUpMenu();
 
     setUpController();
 
     // wait for user's selection of data structure in a loop
-
-    // hide main menu using a helper method
 
     // the rest is mouse events
   }
@@ -80,12 +93,13 @@ public class MyController extends WindowController implements ActionListener {
   private void setUpMenu() {
 
     // the menu instruction for selecting data structure
-    selectionText = new Text("Please choose a data structure:", 0, 0, canvas);
+    selectionText = new Text("Please choose a data structure:", MENU_FONT_SIZE,
+    MENU_FONT_SIZE, canvas);
     selectionText.setFontSize(MENU_FONT_SIZE);
 
     // data structures selection text
-    arrayListText = new Text("Array List", 0, selectionText.getY() +
-    MENU_FONT_SIZE + Y_GAP, canvas);
+    arrayListText = new Text("Array List", selectionText.getX(),
+    selectionText.getY() + MENU_FONT_SIZE + Y_GAP, canvas);
 
     // Text objects can be put into a linkedlist, so this set up can be done in
     // a for loop
@@ -94,8 +108,8 @@ public class MyController extends WindowController implements ActionListener {
 
     // create the instruction text
     instructionText = new Text("Please enter value between 0 to 999 then press "
-    + "corresponding button to add, find, or remove", 0, MENU_FONT_SIZE,
-    canvas);
+    + "corresponding button to add, find, or remove", MENU_FONT_SIZE,
+    MENU_FONT_SIZE, canvas);
     instructionText.setFontSize(MENU_FONT_SIZE / HALF);
     instructionText.setColor(MENU_FONT_COLOR);
     instructionText.hide();
@@ -157,6 +171,58 @@ public class MyController extends WindowController implements ActionListener {
 
     // show the instruction
     instructionText.show();
+
+    // not on menu anymore
+    onMenu = false;
+  }
+
+  /**
+   * menuTextNoHover method, to set every menu text to original menu color.
+   *
+   * @author Jasper
+   */
+  private void menuTextNoHover() {
+
+    // can use for loop
+
+    arrayListText.setColor(MENU_FONT_COLOR);
+
+    textHover = false;
+  }
+
+  /**
+   * checkMousePosition method, to check the current position of the mouse to
+   * determine if the mouse is at a particular data structure text.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   *
+   * @return an int that indicates the data structure text that is being hover,
+   * -1 if none.
+   * 
+   * @author Jasper
+   */
+  private int checkMousePosition(MouseEvent evt) {
+
+    // obtain the mouse's x and y positions
+    int mouseX = evt.getX();
+    int mouseY = evt.getY();
+
+    // obtain the current's text position and length, can use for loop later
+    int textX = (int) arrayListText.getX();
+    int textY = (int) arrayListText.getY();
+    int textWidth = (int) arrayListText.getWidth();
+    int textHeight = (int) arrayListText.getHeight();
+
+    // check if the mouse is within the text box
+    if (mouseX >= textX && mouseX <= textX + textWidth && mouseY >= textY &&
+    mouseY <= textY + textHeight) {
+
+      return ARRAYLIST;
+    }
+
+    // otherwise, return -1
+    return -1;
   }
 
   /**
@@ -175,9 +241,6 @@ public class MyController extends WindowController implements ActionListener {
       // store int to be added
       System.out.println(addTextField.getText());
       addTextField.setText("");
-
-      hideMenu();
-      myDS = new MyArrayList(canvas);
     }
 
     else if (event == findButton) {
@@ -192,6 +255,129 @@ public class MyController extends WindowController implements ActionListener {
       // store int to be removed
       System.out.println(removeTextField.getText());
       removeTextField.setText("");
+    }
+  }
+
+  /**
+   * mouseClicked method, the event when the mouse was clicked.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author Jasper
+   */
+  public void mouseClicked(MouseEvent evt) {
+
+    // if a data structure text is being hover, check the mouse position then
+    // assign a data structure
+    if (textHover) {
+
+      switch (checkMousePosition(evt)) {
+
+        case ARRAYLIST:
+          hideMenu();
+          myDS = new MyArrayList(canvas);
+          break;
+      }
+    }
+  }
+
+  /**
+   * mouseEntered method, the event when the mouse entered the canvas.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author
+   */
+  public void mouseEntered(MouseEvent evt) {
+
+  }
+
+  /**
+   * mouseExited method, the event when the mouse exited the canvas.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author
+   */
+  public void mouseExited(MouseEvent evt) {
+
+  }
+
+  /**
+   * mousePressed method, the event when the mouse was pressed.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author
+   */
+  public void mousePressed(MouseEvent evt) {
+
+  }
+
+  /**
+   * mouseReleased method, the event when the mouse was released.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author
+   */
+  public void mouseReleased(MouseEvent evt) {
+
+  }
+
+  /**
+   * mouseDragged method, the event when the mouse was dragged.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author
+   */
+  public void mouseDragged(MouseEvent evt) {
+
+  }
+
+  /**
+   * mouseMoved method, the event when the mouse was moved.
+   * 
+   * @param MouseEvent evt - the MouseEvent object that contains information
+   * about the current mouse's state.
+   * 
+   * @author Jasper
+   */
+  public void mouseMoved(MouseEvent evt) {
+
+    if (onMenu) {
+
+      // case when text is being hover
+      if (textHover) {
+
+        // then if mouse position is -1, then reset the texts color
+        if (checkMousePosition(evt) == -1) {
+
+          menuTextNoHover();
+        }
+      }
+
+      // case when text is not being hover
+      else {
+
+        switch (checkMousePosition(evt)) {
+
+          case -1:
+            break;
+
+          case ARRAYLIST:
+            arrayListText.setColor(MENU_FONT_HOVER_COLOR);
+            textHover = true;
+            break;
+        }
+      }
     }
   }
 
